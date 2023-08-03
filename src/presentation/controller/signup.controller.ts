f09bc3f6476647b559b452f30fed5864e44d0f26
mabittar <ma_bittar@yaho.com.br>
2023-08-03 20:49:09 -0300

    refactor: add controller interface protocol

--- src/presentation/controller/signup.controller.ts
+++ src/presentation/controller/signup.controller.ts
@@ -1,8 +1,10 @@
 import { MissingParamError } from "./errors/missing-param-error"
 import { badRequest } from "./helpers/http-helper"
+import { ControllerInterface } from "./protocols/controller.interface"
+import { HttpRequestProtocol, HttpResponseProtocol } from "./protocols/http"
 
-export class SignUpController {
-  handle ( httpRequest: any): any{
+export class SignUpController implements ControllerInterface {
+  handle ( httpRequest: HttpRequestProtocol): HttpResponseProtocol{
     const requiredFields: string[] = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
